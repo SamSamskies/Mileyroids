d524a5bf494eb0348f717a3fc71f264c1754ccb4
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-02-06 16:26:53 -0800

    Add instructions

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var APP = {
   startGame: function() {
     var s = $('<script/>').attr('src', 'js/kickass.js');
     $('body').append(s);
+    $('#mask').hide();
+    $('#instructions-container').hide();
   }
 }
 
