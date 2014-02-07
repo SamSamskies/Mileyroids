6607e1bdbbfb1dc79f1aefa36536af9e2341d214
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-02-07 01:20:56 -0800

    Dynamically place images on the screen

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var APP = {
   init: function() {
     $(document).konami_code({ callback: APP.startGame, maximum: 1 });
     APP.loadYouTubeAPIAsyc();
+    APP.$imagesContainer = $('#images-container');
+    APP.seedRandomImages(50);
   },
 
   // This code loads the IFrame Player API code asynchronously.
@@ -12,19 +14,33 @@ var APP = {
      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
   },
 
+  randomX: function() {
+    return Math.random() * $(window).width();
+  },
+
+  randomY: function() {
+    return Math.random() * $(window).height();
+  },
+
+  seedRandomImages: function(numOfImages) {
+    for(var i=0; i < numOfImages; i++) {
+      var img = '<img src="images/' + Math.floor((Math.random() * 8) + 1) + '.gif" ' +
+                'style="top:' + APP.randomY() + 'px; left:' + APP.randomX() + 'px;">'
+      APP.$imagesContainer.append(img)
+    }
+  },
+
   startGame: function() {
     var s = $('<script/>').attr('src', 'js/kickass.js');
     $('body').append(s);
     $('#mask').hide();
     $('#instructions-container').hide();
-    APP.player.playVideo();
     APP.miniPlayer.playVideo();
   }
 }
 
 // This stuff needs to be global
 function onYouTubeIframeAPIReady() {
-  APP.player = new YT.Player($('#main-vid')[0]);
   APP.miniPlayer = new YT.Player($('#mini-vid')[0])
 }
 
